5f6e73d0f0f5ccb48c396a0c98ec98566d77aa9d
zz <zhuguang@ejoy.com>
2016-06-07 13:40:11 +0800

    [ADDED] android makefile

---
+++ Android.mk
@@ -0,0 +1,22 @@
+INNER_SAVED_LOCAL_PATH := $(LOCAL_PATH)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ps
+
+LOCAL_CFLAGS := -std=gnu99
+
+LOCAL_C_INCLUDES  := \
+	${LUA_SRC_PATH} \
+	${EJOY2D_SRC_PATH} \
+	${EJOY2D_ADAPTER_SRC_PATH} \
+	${DTEX_SRC_PATH} \
+
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
+
+include $(BUILD_STATIC_LIBRARY)	
+
+LOCAL_PATH := $(INNER_SAVED_LOCAL_PATH)
